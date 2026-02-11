caa822d7a1364ff48941351e5cf696d30794ad5b
Jean P. Cebinel <92955454+JeanPCB@users.noreply.github.com>
2026-02-11 04:18:48 +0000

    Refactor validation and remove redundancy

--- index.js
+++ index.js
@@ -11,24 +11,17 @@ function mult(a, b) {
 }
 
 function div(a, b) {
-	return a / b;
+	if (b > 0) return a / b;
 }
 
 function main() {
 	const operations = { add, sub, mult, div };
 	const opArg = process.argv[2];
-	const operand1 = Number(process.argv[3]);
-	const operand2 = Number(process.argv[4]);
+	const operand1 = Number(process.argv[3] ?? 0);
+	const operand2 = Number(process.argv[4] ?? 0);
 
-	if (operand1 && operand2) {
-		let result = 0;
-
-		for (const operation in operations) {
-			if (opArg === operation) {
-				result = operations[opArg](operand1, operand2);
-			}
-		}
-
+	if (operations[opArg]) {
+		const result = operations[opArg](operand1, operand2);
 		console.log(result);
 	}
 }
