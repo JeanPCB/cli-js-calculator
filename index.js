b679571f050169d9552c241990b19d63661ab493
Jean P. Cebinel <92955454+JeanPCB@users.noreply.github.com>
2026-02-11 03:17:28 +0000

    Add arguments validation and print operation result

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ function div(a, b) {
 	return a / b;
 }
 
-const operations = { add, sub, mult, div };
+function main() {
+	const operations = { add, sub, mult, div };
+	const opArg = process.argv[2];
+	const operand1 = Number(process.argv[3]);
+	const operand2 = Number(process.argv[4]);
+
+	if (operand1 && operand2) {
+		let result = 0;
+
+		for (const operation in operations) {
+			if (opArg === operation) {
+				result = operations[opArg](operand1, operand2);
+			}
+		}
+
+		console.log(result);
+	}
+}
+
+main();
